9b53d38f46360d5492a44d6d199f7da8d7fbdc68
bilol0403 <abdulxaqovbilol@gmail.com>
2023-01-15 13:52:01 +0500

    js work one

---
+++ script.js
@@ -0,0 +1,16 @@
+let a =  10;
+let b = 5;
+let c = a - b;
+console.log(c);
+let d = 7;
+let result = c + d;
+console.log(result);
+ a = 10;
+ b = 3;
+ console.log(a % b);
+ console.log(2**10);
+ console.log(Math.pow(2, 10));
+
+
+// let nickName = prompt("what is your insta nickname?");
+// console.log(`https//www.instagram.com/${nickName}`);
